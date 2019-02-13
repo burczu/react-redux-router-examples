a1e53b77054c6ee0fee379d097822b3d30cbb73e
Bartomiej Dybowski <b.dybowski@gmail.com>
2019-02-13 01:31:36 +0100

    switch explained

--- src/components/routing-switch.js
+++ src/components/routing-switch.js
@@ -13,22 +13,27 @@ const App = () => {
     <Router>
       <div>
         <Switch>
+          // dzięki Switch, pierwszy pasujący komponent zostanie
+          // wyrenderowany, a kolejne pominięte
           <Route exact path="/" component={Home} />
+
+          // "special" pasuje do ":itemId" ale dzięki temu, że jest wcześniej
+          // i oba są owinięte przez Switch, wyrenderuje się tylko komponent SpecialItem
           <Route path="/items/special" component={SpecialItem} />
           <Route path="/items/:itemId" component={Items} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 
 const Home = () => {
   return <h1>Home!!</h1>;
-}
+};
 
 const SpecialItem = () => {
   return <h1>Specia Item!!</h1>;
-}
+};
 
 const Items = (props) => {
   return (
@@ -37,7 +42,6 @@ const Items = (props) => {
       <p>Item id = {props.match.params.itemId}</p>
     </div>
   );
-}
-}
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
